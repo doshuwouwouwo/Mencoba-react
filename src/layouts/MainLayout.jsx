51105146743ef49b2186918ee00dd5f0cdd34348
fir d aus <firdausramadhana02@gmail.com>
2025-01-02 18:41:29 +0700

    setup project react & vite

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+
+import Navbar from "../components/Navbar";
+
+
+export default function MainLayout({ children }) {
+  return (
+    <div>
+        <Navbar />
+        {children}
+    </div>
+  )
+}
